ad92cae916e04c80d3ff5e352d22a229c8173e76
LyleMijares <lylemijares@gmail.com>
2026-06-11 00:02:08 +0800

    Integrate Firestore to save user data on registration
    Added Firestore integration to store user data after creation.

--- Services/AuthServices.cs
+++ Services/AuthServices.cs
@@ -2,6 +2,8 @@ using System;
 using System.Threading.Tasks;
 using FirebaseAdmin;
 using FirebaseAdmin.Auth;
+using Google.Cloud.Firestore;
+using System.Collections.Generic;
 
 namespace haru.market.Services
 {
@@ -14,16 +16,40 @@ namespace haru.market.Services
                 Email = email,
                 Password = password,
                 DisplayName = username,
-                Disabled = false,
+                Disabled = false
             };
 
-            return await FirebaseAuth.DefaultInstance.CreateUserAsync(args);
+            UserRecord user = await FirebaseAuth.DefaultInstance.CreateUserAsync(args);
+
+            DocumentReference userDoc =
+                _firestoreDb
+                    .Collection("users")
+                    .Document(user.Uid);
+
+            var firestoreUser = new Dictionary<string, object>
+            {
+                { "uid", user.Uid },
+                { "email", email },
+                { "username", username },
+                { "createdAt", Timestamp.GetCurrentTimestamp() }
+            };
+
+            await userDoc.SetAsync(firestoreUser);
+
+            return user;
         }
 
         // us 03 for login persistence
         public async Task<string> CreateSessionTokenAsync(string uid)
         {
             return await FirebaseAuth.DefaultInstance.CreateCustomTokenAsync(uid);
-            }
+        }
+
+        private readonly FirestoreDb _firestoreDb;
+
+        public AuthService(FirestoreDb firestoreDb)
+        {
+            _firestoreDb = firestoreDb;
         }
     }
+}
