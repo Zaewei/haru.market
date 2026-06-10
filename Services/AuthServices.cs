70f589b2fee290a2119789e7a5047b5b890b143c
LyleMijares <lylemijares@gmail.com>
2026-06-11 00:00:52 +0800

    Add Firestore integration to user registration
    Enhance user registration by saving user data to Firestore.

--- Services/AuthServices.cs
+++ Services/AuthServices.cs
@@ -2,28 +2,54 @@ using System;
 using System.Threading.Tasks;
 using FirebaseAdmin;
 using FirebaseAdmin.Auth;
+using Google.Cloud.Firestore;
+using System.Collections.Generic;
 
 namespace haru.market.Services
 {
     public class AuthService
     {
-        public async Task<UserRecord> RegisterUserAsync(string email, string password, string username)
+        public async Task<UserRecord> RegisterUserAsync(string email,  string password, string username)
         {
             var args = new UserRecordArgs()
             {
                 Email = email,
                 Password = password,
                 DisplayName = username,
-                Disabled = false,
+                Disabled = false
             };
 
-            return await FirebaseAuth.DefaultInstance.CreateUserAsync(args);
+            UserRecord user = await FirebaseAuth.DefaultInstance.CreateUserAsync(args);
+
+            DocumentReference userDoc =
+                _firestoreDb
+                    .Collection("users")
+                    .Document(user.Uid);
+
+            var firestoreUser = new Dictionary<string, object>
+            {
+                { "uid", user.Uid },
+                { "email", email },
+                { "username", username },
+                { "createdAt", Timestamp.GetCurrentTimestamp() }
+            };
+
+            await userDoc.SetAsync(firestoreUser);
+
+            return user;
         }
 
         // us 03 for login persistence
         public async Task<string> CreateSessionTokenAsync(string uid)
         {
             return await FirebaseAuth.DefaultInstance.CreateCustomTokenAsync(uid);
-            }
+        }
+
+        private readonly FirestoreDb _firestoreDb;
+
+        public AuthService(FirestoreDb firestoreDb)
+        {
+            _firestoreDb = firestoreDb;
         }
     }
+}
